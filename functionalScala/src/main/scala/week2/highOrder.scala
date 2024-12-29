e9fba6a0d77f3e63c5de82769a42b264209e465c
Kaleb Roncatti <kalebroncatti@gmail.com>
2024-12-29 18:44:27 +0000

    feat: currying for sum

--- functionalScala/src/main/scala/week2/highOrder.scala
+++ functionalScala/src/main/scala/week2/highOrder.scala
@@ -9,7 +9,7 @@ def sum(f: Int => Int, a: Int, b: Int): Int =
 
 def sumInts(a: Int, b: Int) = sum((x: Int) => x, a, b)
 
-// Tail recursion version
+// Tail recursion version (Cheaper)
 def sumTR(f: Int => Int, a: Int, b: Int): Int =
   @tailrec
   def loop(a: Int, acc: Int): Int =
@@ -17,6 +17,16 @@ def sumTR(f: Int => Int, a: Int, b: Int): Int =
     else loop(a + 1, acc + f(a))
   loop(a, 0)
 
-def sumCubes(a: Int, b: Int) = sumTR((x: Int) => x * x * x, a, b)
+// def sumCubes(a: Int, b: Int) = sumTR((x: Int) => x * x * x, a, b)
+
+// Going deeper and using currying
+// This will hide a/b and the sum function will return another function.
+def sumCurrying(f: Int => Int): (Int, Int) => Int =
+  def sumF(a: Int, b: Int) : Int =
+    if a > b then 0
+    else f(a) + sumF(a + 1, b)
+  sumF
+
+def sumCubes = sumCurrying((x: Int) => x * x * x)
 
 @main def test: Unit = println(sumCubes(2, 4))
