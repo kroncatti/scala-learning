5ac5fb91c62c7ddb04614e0ec7a7908cc7156b45
Kaleb Roncatti <kalebroncatti@gmail.com>
2024-12-29 20:25:25 +0000

    feat: fix rational

--- functionalScala/src/main/scala/week2/Rational.scala
+++ functionalScala/src/main/scala/week2/Rational.scala
@@ -6,17 +6,25 @@ class Rational(x: Int, y: Int):
   private def numerator: Int = x
   private def denominator: Int = y
 
-  def add(r: Rational) : Rational =
+  def add(r: Rational): Rational =
     Rational(
       numerator * r.denominator + r.numerator * denominator,
       denominator * r.denominator
     )
 
+  def multiply(r: Rational): Rational =
+    Rational(
+      numerator * r.numerator,
+      denominator * r.denominator
+    )
+
   override def toString = s"${numerator}/${denominator}"
+  
+end Rational
 
 
 val rat = Rational(2, 3)
-val rat2 = Rational(4, 5)
-val rat3 = rat add rat2
+val rat2 = rat add Rational(1, 3)
+val rat3 = rat2 multiply Rational(2, 4)
 
 @main def testRational: Unit = println(rat3.toString())
