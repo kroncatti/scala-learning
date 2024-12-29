34e9f58db7a407161c2f56d4c16660ac6cf61d9f
Kaleb Roncatti <kalebroncatti@gmail.com>
2024-12-29 18:58:30 +0000

    feat: currying

--- functionalScala/src/main/scala/week2/highOrder.scala
+++ functionalScala/src/main/scala/week2/highOrder.scala
@@ -2,6 +2,10 @@ package week2
 
 import scala.annotation.tailrec
 
+//
+// HIGH ORDER FUNCTIONS
+//
+
 // Linear recursion version
 def sum(f: Int => Int, a: Int, b: Int): Int =
   if a > b then 0
@@ -9,7 +13,10 @@ def sum(f: Int => Int, a: Int, b: Int): Int =
 
 def sumInts(a: Int, b: Int) = sum((x: Int) => x, a, b)
 
-// Tail recursion version (Cheaper)
+//
+// TAIL RECURSION VERSION
+//
+
 def sumTR(f: Int => Int, a: Int, b: Int): Int =
   @tailrec
   def loop(a: Int, acc: Int): Int =
@@ -17,9 +24,12 @@ def sumTR(f: Int => Int, a: Int, b: Int): Int =
     else loop(a + 1, acc + f(a))
   loop(a, 0)
 
-// def sumCubes(a: Int, b: Int) = sumTR((x: Int) => x * x * x, a, b)
+def sumCubes(a: Int, b: Int) = sumTR((x: Int) => x * x * x, a, b)
 
-// Going deeper and using currying
+//
+// CURRYING MECHANISM
+//
+
 // This will hide a/b and the sum function will return another function.
 def sumCurrying(f: Int => Int): (Int, Int) => Int =
   def sumF(a: Int, b: Int) : Int =
@@ -27,6 +37,10 @@ def sumCurrying(f: Int => Int): (Int, Int) => Int =
     else f(a) + sumF(a + 1, b)
   sumF
 
-def sumCubes = sumCurrying((x: Int) => x * x * x)
+def sumCubesCurrying = sumCurrying((x: Int) => x * x * x)
 
-@main def test: Unit = println(sumCubes(2, 4))
+// Shorter version of currying using a multi-parameter list and "jumping" some steps
+def sumMultipleParameters(f: Int => Int)(a: Int, b: Int): Int =
+  if a > b then 0 else f(a) + sumMultipleParameters(f)(a + 1, b)
+
+@main def test: Unit = println(sumMultipleParameters(x => x * x * x)(2, 4))
